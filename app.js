7b4c421154ea166aa05fe0c5da1183ab2d3f19ee
aditya <adityatandulkar20@gmail.com>
2023-11-01 22:47:56 +0530

    update 1

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const modal = document.getElementsByClassName("modal-wrapper");
 function openModal(name = "", mrp = ""){
     modal[0].classList.remove("hidden");
     if (name != "" && mrp != "") {
-        document.getElementsByClassName("mrp-wrapper")[0].innerHTML = name;
-        document.getElementsByClassName("mrp-wrapper")[1].innerHTML = mrp;
+        document.getElementById("name").innerHTML = name;
+        document.getElementById("mrp").innerHTML = mrp;
     }
 };
 
